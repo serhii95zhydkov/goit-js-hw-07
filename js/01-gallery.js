5133818bbaa2908a571fa314b6f835619b6c36cb
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-12 11:58:47 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const getGalleryItems = ({preview, original, description}) => `<div class="gallery__item">
+const createModal = (modalImageSrc) => {
+  const modal = basicLightbox.create(`
+    <img src=${modalImageSrc} />
+`);
+  return modal;
+}; 
+
+const refs = {
+  gallery: document.querySelector(`.gallery`),
+};
+
+const getGalleryItems = ({ preview, original, description }) => `<div class="gallery__item">
   <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
@@ -12,12 +23,22 @@ const getGalleryItems = ({preview, original, description}) => `<div class="galle
   </a>
 </div>`;
 
-const refs = {
-    gallery: document.querySelector(`.gallery`),
-};
-
 const listGalleryItems = galleryItems.map((item) => getGalleryItems(item)).join(``);
 
 refs.gallery.insertAdjacentHTML(`beforeend`, listGalleryItems);
+
+const onClickOpenModal = (event) => {
+  event.preventDefault();
+  
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  };
+
+  const modalImageSrc = event.target.dataset.source;
+
+  createModal(modalImageSrc).show();
+};
+
+refs.gallery.addEventListener("click", onClickOpenModal);
 
 console.log(galleryItems);
